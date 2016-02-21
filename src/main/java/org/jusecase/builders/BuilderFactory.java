72d80994411ba7ae3e49a01fb8a016b77b5116af
codejanovic <codejanovic@gmail.com>
2016-02-21 14:18:58 +0100

    added BuilderFactory tests

--- src/main/java/org/jusecase/builders/BuilderFactory.java
+++ src/main/java/org/jusecase/builders/BuilderFactory.java
@@ -4,6 +4,7 @@ import org.jusecase.builders.builders.collections.*;
 import org.jusecase.builders.builders.collections.CollectionBuilder;
 import org.jusecase.builders.builders.misc.DateBuilder;
 import org.jusecase.builders.builders.streams.ResourceInputStreamBuilder;
+import org.jusecase.builders.builders.streams.StringInputStreamBuilder;
 import org.jusecase.builders.newables.NewableArrayList;
 
 import java.util.Collection;
@@ -26,28 +27,32 @@ public class BuilderFactory {
         return entities;
     }
 
-    public static <T, TCollectionType extends Collection<T>> org.jusecase.builders.builders.collections.CollectionBuilder<T, TCollectionType> createCollectionBuilder(){
-        return new CollectionBuilder<T, TCollectionType>(new NewableArrayList());
+    public static <T, TCollectionType extends Collection<T>> CollectionBuilder<T, TCollectionType> createCollectionBuilder(final Newable<TCollectionType> newable){
+        return new CollectionBuilder<T, TCollectionType>(newable);
     }
 
-    public static <T, TCollectionType extends Collection<T>> CopyCollectionBuilder<T, TCollectionType> createCopyCollectionBuilder(){
-        return new CopyCollectionBuilder<T, TCollectionType>(new NewableArrayList());
+    public static <T, TCollectionType extends Collection<T>> CopyCollectionBuilder<T, TCollectionType> createCopyCollectionBuilder(final Newable<TCollectionType> newable){
+        return new CopyCollectionBuilder<T, TCollectionType>(newable);
     }
 
-    public static <T, TCollectionType extends Collection<T>> AggregateCollectionBuilder<T, TCollectionType> createAggregateCollectionBuilder(){
-        return new AggregateCollectionBuilder<T, TCollectionType>(new NewableArrayList());
+    public static <T, TCollectionType extends Collection<T>> AggregateCollectionBuilder<T, TCollectionType> createAggregateCollectionBuilder(final Newable<TCollectionType> newable){
+        return new AggregateCollectionBuilder<T, TCollectionType>(newable);
     }
 
-    public static <T, TCollectionType extends Collection<T>> UnmodifiableCollectionBuilder<T, TCollectionType> createUnmodifiableCollectionBuilder(){
-        return new UnmodifiableCollectionBuilder<T, TCollectionType>(new NewableArrayList());
+    public static <T, TCollectionType extends Collection<T>> UnmodifiableCollectionBuilder<T, TCollectionType> createUnmodifiableCollectionBuilder(final Newable<TCollectionType> newable){
+        return new UnmodifiableCollectionBuilder<T, TCollectionType>(newable);
     }
 
-    public static <T, TCollectionType extends Collection<T>> ImmutableCollectionBuilder<T, TCollectionType> createImmutableCollectionBuilder(){
-        return new ImmutableCollectionBuilder<T, TCollectionType>(new NewableArrayList());
+    public static <T, TCollectionType extends Collection<T>> ImmutableCollectionBuilder<T, TCollectionType> createImmutableCollectionBuilder(final Newable<TCollectionType> newable){
+        return new ImmutableCollectionBuilder<T, TCollectionType>(newable);
     }
 
-    public static ResourceInputStreamBuilder createInputStreamBuilder() {
+    public static ResourceInputStreamBuilder createResourceInputStreamBuilder() {
         return new ResourceInputStreamBuilder();
+    }
+
+    public static StringInputStreamBuilder createStringInputStreamBuilder() {
+        return new StringInputStreamBuilder();
     }
 
     public static DateBuilder createDateBuilder() {
