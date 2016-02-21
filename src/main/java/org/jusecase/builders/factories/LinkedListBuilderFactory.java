4356a4c64ac7de225f521255fabda627fbb2b061
codejanovic <codejanovic@gmail.com>
2016-02-21 18:45:44 +0100

    added LinkedListBuilderFactory tests

--- src/main/java/org/jusecase/builders/factories/LinkedListBuilderFactory.java
+++ src/main/java/org/jusecase/builders/factories/LinkedListBuilderFactory.java
@@ -1,14 +1,13 @@
 package org.jusecase.builders.factories;
 
-import org.jusecase.builders.builders.collections.AggregateCollectionBuilder;
-import org.jusecase.builders.builders.collections.CollectionBuilder;
-import org.jusecase.builders.builders.collections.CopyCollectionBuilder;
+import org.jusecase.builders.builders.collections.*;
+import org.jusecase.builders.newables.NewableArrayList;
 import org.jusecase.builders.newables.NewableLinkedList;
 
 import java.util.List;
 
 public class LinkedListBuilderFactory {
-    public static <T> CollectionBuilder<T, List<T>> linkedList(final T... items){
+    public static <T> CollectionBuilder<T, List<T>> list(final T... items){
         return new CollectionBuilder<T, List<T>>(new NewableLinkedList()).with(items);
     }
 
@@ -18,5 +17,13 @@ public class LinkedListBuilderFactory {
 
     public static <T> AggregateCollectionBuilder<T, List<T>> aggregate(final List<T>... linkedLists){
         return new AggregateCollectionBuilder<T, List<T>>(new NewableLinkedList()).with(linkedLists);
+    }
+
+    public static <T> UnmodifiableCollectionBuilder<T, List<T>> unmodifiable(final List<T> list){
+        return new UnmodifiableCollectionBuilder<T, List<T>>(new NewableLinkedList()).with(list);
+    }
+
+    public static <T> ImmutableCollectionBuilder<T, List<T>> immutable(final List<T> list){
+        return new ImmutableCollectionBuilder<T, List<T>>(new NewableLinkedList()).with(list);
     }
 }
